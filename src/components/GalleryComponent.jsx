3cffb61f93c8769de5d0bb5a17ea4c33fbdc0c60
Sunhaha520 <67938343+Sunhaha520@users.noreply.github.com>
2024-10-04 18:04:34 +0800

    Update GalleryComponent.jsx

--- src/components/GalleryComponent.jsx
+++ src/components/GalleryComponent.jsx
@@ -1,77 +1,25 @@
 import * as React from "react";
 import Lightbox from "yet-another-react-lightbox";
-import "yet-another-react-lightbox/styles.css";
-import Masonry from "react-masonry-css";
+import { RowsPhotoAlbum } from "react-photo-album";
+import "react-photo-album/rows.css";
 
 export default function GalleryComponent({ images }) {
-  const [open, setOpen] = React.useState(false);
-  const [currentSlide, setCurrentSlide] = React.useState(0);
-  const [hoveredImage, setHoveredImage] = React.useState(null);
-
-  const slides = images.map((image) => ({ src: image.src, title: image.title, description: image.description }));
-
-  const openLightbox = (index) => {
-    setCurrentSlide(index);
-    setOpen(true);
-  };
-
-  const handleMouseEnter = (image) => {
-    setHoveredImage(image);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredImage(null);
-  };
+  const [index, setIndex] = React.useState(-1);
 
   return (
     <>
-      <Masonry
-        breakpointCols={{ default: 3, 800: 2, 400: 1 }}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column"
-      >
-        {images.map((image, index) => (
-          <div
-            key={index}
-            onClick={() => openLightbox(index)}
-            onMouseEnter={() => handleMouseEnter(image)}
-            onMouseLeave={handleMouseLeave}
-          >
-            <img src={image.src} alt={`Image ${index}`} style={{ cursor: "pointer" }} />
-            {hoveredImage === image && (
-              <div className="image-info">
-                <h3>{image.title}</h3>
-                <p>{image.description}</p>
-              </div>
-            )}
-          </div>
-        ))}
-      </Masonry>
-
-      <Lightbox
-        open={open}
-        close={() => setOpen(false)}
-        slides={slides}
-        index={currentSlide}
+      <RowsPhotoAlbum
+        photos={images}
+        targetRowHeight={150}
+        onClick={({ index: current }) => setIndex(current)}
       />
 
-      <style jsx>{`
-        .image-info {
-          position: absolute;
-          bottom: 0;
-          left: 0;
-          right: 0;
-          background: rgba(0, 0, 0, 0.7);
-          color: white;
-          padding: 10px;
-          opacity: 0;
-          transition: opacity 0.3s ease;
-        }
-
-        .my-masonry-grid_column > div:hover .image-info {
-          opacity: 1;
-        }
-      `}</style>
+      <Lightbox
+        index={index}
+        slides={images}
+        open={index >= 0}
+        close={() => setIndex(-1)}
+      />
     </>
   );
 }
